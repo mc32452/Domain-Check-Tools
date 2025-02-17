461a9a1e87d849960d77db050f71d610d6d2c545
Misha M <157011371+mc32452@users.noreply.github.com>
2025-02-17 12:22:46 +0000

    Update main.py: moved logo

--- main.py
+++ main.py
@@ -283,7 +283,7 @@ async def run_all_in_one_checks(domains, timeout, concurrency, retries, dns_reco
     return results
 
 st.set_page_config(page_title="Domain Checker", layout="wide")
-st.title("🔍Domain Checker")
+st.title("Domain Checker🔍")
 
 tabs = st.tabs(["HTTP Check", "DNS Lookup", "WHOIS Check", "TLS/SSL Certificate Check", "Advanced Check"])
 
